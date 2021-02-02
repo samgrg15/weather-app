861d42d0d3c0a9894aaf9d90cc91564a3552a8af
samgrg15 <78025757+samgrg15@users.noreply.github.com>
2021-02-02 11:24:04 +0000

    Added package.json

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 let axios = require("axios");
-
 let now = new Date();
 let hours = now.getHours();
 let minutes = now.getMinutes();
-
 let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Friday", "Sat"];
 let date = week[now.getDay()];
-
 let dayTime = `${date} ${hours}:${minutes}`;
 document.querySelector("#current-day-time").innerHTML = `<p>${dayTime}</p>`;
-
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
   console.log(searchInput);
-
   let h2 = document.querySelector("h2");
   h2.innerHTML = searchInput.value;
 }
-
 let element = document.querySelector("#city-submit-button");
 element.addEventListener("click", showCity);
-
-function searchCity(location) {
+function searchCity() {
+  let searchCity = document.querySelector("#search-text-input");
   let apiKey = "3e0a34619942fda75b63ed8d41cdcd36";
-  let city = document.querySelector("#search-text-input");
-  console.log(city);
-  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
-
-  axios.get(apiUrl).then(searchCity);
+  console.log(searchCity);
+  axios.get(apiUrl).then(function(response){
+    let temperature = Math.round(response.data.main.temp);
+    console.log(response.data.main.temp);
+    document.querySelector("h3").innerHTML = `${temperature}°C`;
+    let feelsLike = Math.round(response.data.main.feels_like);
+    document.querySelector(".feels").innerHTML = `Feels like ${feelsLike}°C`;
+  });
 }
+let sam = document.querySelector("#city-submit-button");
+sam.addEventListener("click", searchCity);
